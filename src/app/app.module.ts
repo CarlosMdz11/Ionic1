6cccc2a85ef81434f7d5af91038d52f9e5b3480e
Carlos Ivan Mendez <carlosivanmendez16@gmail.com>
2020-12-18 15:20:45 -0600

    Mi primer cambio en Ionic 6

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,7 +14,7 @@ import { AppComponent } from './app.component';
   entryComponents: [],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
   providers: [
-    StatusBar,
+    StatusBar, 
     SplashScreen,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
   ],
